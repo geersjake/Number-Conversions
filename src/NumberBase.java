6fd351740f19cc3e1a03c19c3133f8c1c99d454b
geersjake <geersjake@gmail.com>
2016-09-06 20:41:51 -0500

    working on invalid input and testing

--- src/NumberBase.java
+++ src/NumberBase.java
@@ -1,137 +1,205 @@
 import java.math.BigInteger;
 import java.util.ArrayList;
 
-/**
- Created by Jake on 9/2/2016.
- */
+/*****************************************************************
+ This class converts a number between two bases ranging from 2-36
+
+ @author Jake Geers
+ @version 9/6/2016
+ *****************************************************************/
 public class NumberBase {
 
-
-
+    /*****************************************************************
+     Central method used to convert bases. Calls various other methods
+     to help convert.
+     @param input the string representation of number to convert
+     @param base_in the base that input is in currently
+     @param base_out the base which input will be converted into
+     @throws IllegalArgumentException when params are improper
+     @return String representation of converted input
+     *****************************************************************/
     public static String convert (String input, int base_in, int base_out){
+        //checks input
+        if (isValidInput(input, base_in) == false){
+            throw new IllegalArgumentException();
+        }
+
+        //creates array of bigInts via convertIntoArray method
         ArrayList<BigInteger> nums = convertIntoArray(input);
+
+        //transforms nums array into a single bigInt
         BigInteger input_as_integer = toInteger(nums);
-        String ans = "miss";
+
+        //returned string
+        String ans;
+
+        //variable used to temporarily hold converted input
         BigInteger sum;
-        if (base_out == 10){
+
+
+        if (base_out == 10){ //checking if base out is base10
+            //sets sum to method return of toBaseTen
             sum = toBaseTen(nums,BigInteger.valueOf(base_in));
-            ans = sum + "";
-            ans.trim();
+            ans = sum + ""; //quotes used for type conversion
+            ans.trim(); //eliminates whitespace created above
 
         }
-        else if (base_in != 10){
+        else if (base_in != 10){ //detects if base in is anything but 10
+            //converts input to base ten
             BigInteger input_in_base_ten = toBaseTen(nums, BigInteger.valueOf(base_in));
 
+            //necessary for recursion in toOtherBase method
             ArrayList<BigInteger> temp = new ArrayList<>();
-            ans = toBaseTwo(input_in_base_ten, BigInteger.valueOf(base_out), temp);
 
-        } else {
+            //now that input is in base ten, converts to desired base
+            ans = toOtherBase(input_in_base_ten, BigInteger.valueOf(base_out), temp);
+
+        } else { //catches case in-which baseIn is 10
+            //necessary for recursion in toOtherBase method
             ArrayList<BigInteger> temp = new ArrayList<>();
-            ans = toBaseTwo(input_as_integer, BigInteger.valueOf(base_out), temp);
+
+            //since baseIn is 10 we skip converting toBaseTen
+            ans = toOtherBase(input_as_integer, BigInteger.valueOf(base_out), temp);
         }
 
-        if (ans.startsWith("0")){
+        if (ans.startsWith("0")){ //checking for leading zeros
+            //regex that removes all leading zeros unless needed i.e ans = 0
+            //regex taken from stack overflow thread
             ans = ans.replaceFirst("^0+(?!$)", "");
         }
 
         return ans;
     }
 
-    public static String toBaseTwo (BigInteger input, BigInteger base_out,ArrayList<BigInteger> remainders ){
-        remainders.add(input.mod(base_out));
-        if (input.compareTo(BigInteger.ZERO) == 0){
-            System.out.println(remainders);
-            return toLetters(remainders);
-        }
-        else {
-            return toBaseTwo(input.divide(base_out), base_out, remainders);
-        }
-
-
-
-    }
-
-    public static BigInteger toInteger (ArrayList<BigInteger> nums){
+    /*****************************************************************
+     Converts any input in any base into base 10
+     @param nums arrayList which holds each digit of initial input
+     @param base_in base which input was initially
+     @return number converted to base 10
+     *****************************************************************/
+    public static BigInteger toBaseTen (ArrayList<BigInteger> nums, BigInteger base_in){
         BigInteger sum = BigInteger.valueOf(0);
-        BigInteger num_place = BigInteger.valueOf(1);
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums.get(i).compareTo(BigInteger.ZERO) == 0){
-                num_place = num_place.multiply(BigInteger.valueOf(10));
-
-            }
-            else {
-                sum = sum.add(nums.get(i).multiply(num_place));
-                num_place = num_place.multiply(BigInteger.valueOf(10));
-            }
-
+        for (int i = 0; i < nums.size(); i++) { //cycles through array
+            //multiplies nums value at i by the baseIn raised to the
+            // i'th power and then adds to sum
+            sum = sum.add(nums.get(i).multiply(base_in.pow(i)));
         }
         return sum;
     }
 
+    /*****************************************************************
+     Converts input into any base besides base 10
+     @param input bigInt representation of original input string
+     @param base_out base to which to convert
+     @param remainders holds remainders for use in toLetters method
+     @return converted base as a string
+     *****************************************************************/
+    public static String toOtherBase(BigInteger input, BigInteger base_out, ArrayList<BigInteger> remainders ){
+        remainders.add(input.mod(base_out)); //adding remainder to array
+
+        //exits recursion when modular division reaches zero
+        if (input.compareTo(BigInteger.ZERO) == 0){
+            //sends remainder array to be converted into a string
+            return toLetters(remainders);
+        } else {
+            //recursive call sends input divided by baseOut and retains list of remainders
+            return toOtherBase(input.divide(base_out), base_out, remainders);
+        }
+    }
+
+    /*****************************************************************
+     Converts an arrayList of bigInts into a single bigInt
+     @param nums an arrayList that represents each digit of the
+     original input
+     @return the bigInt value of the original input
+     *****************************************************************/
+    public static BigInteger toInteger (ArrayList<BigInteger> nums){
+        BigInteger sum = BigInteger.valueOf(0);
+
+        //used to convert a value in the arry to the correct number place
+        BigInteger num_place = BigInteger.valueOf(1);
+
+        for (int i = 0; i < nums.size(); i++) { //cycles through array
+
+            //if the value in the array at i is a zero, multiplying by
+            //the num_place would result in zero. This statement prevents
+            //this by ignoring the zero and continuing through loop
+            if (nums.get(i).compareTo(BigInteger.ZERO) == 0){
+                num_place = num_place.multiply(BigInteger.valueOf(10));
+            } else {
+                //multiplies value at i with its number place and adds it to sum
+                sum = sum.add(nums.get(i).multiply(num_place));
+                num_place = num_place.multiply(BigInteger.valueOf(10));
+            }
+        }
+        return sum;
+    }
+
+    /*****************************************************************
+     Converts an array of bigInts into a string that represents the
+     array in the correct value based on base
+     @param nums arrayList of bigInts holding the value to be transformed
+     @return a string of letters and numbers representing converted base
+     *****************************************************************/
     public static String toLetters (ArrayList<BigInteger> nums){
         String ans = "";
-
-        String[] letters = {
-            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h",
+        String[] letters = { //array holding sequence of values used to go up to base 36
+                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h",
                 "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
         };
-
-        for (int i = 0; i < nums.size(); i++) {
-
-            ans = letters[nums.get(i).intValue()] + ans;
-
+        for (int i = 0; i < nums.size(); i++) { //cycles through array
+            ans = letters[nums.get(i).intValue()] + ans; //adding values to string in reverse order
         }
         return ans;
     }
 
-    public static BigInteger toBaseTen (ArrayList<BigInteger> nums, BigInteger base_in){
-        BigInteger sum = BigInteger.valueOf(0);
-
-        for (int i = 0; i < nums.size(); i++) {
-            sum = sum.add(nums.get(i).multiply(base_in.pow(i)));
-
-
-        }
-
-        return sum;
-    }
-
-
+    /*****************************************************************
+     Converts original input in the form of a string into an array with
+     each element added in reverse order
+     @param input string representation of original input in base baseIn
+     @return an arrayList of bigInts whos elemnts hold a single digit
+     of the original input in reverse order
+     *****************************************************************/
     public static ArrayList<BigInteger> convertIntoArray (String input){
         ArrayList<BigInteger> nums = new ArrayList<>();
-        char [] charDigits = input.toCharArray();
-        for (int i = charDigits.length-1; i >= 0; i--) {
+        char [] charDigits = input.toCharArray(); //converting to array
+        for (int i = charDigits.length-1; i >= 0; i--) { //cycles through in reverse order
+            //converts char to int to bigInt and adds to nums array
             nums.add(BigInteger.valueOf(Character.getNumericValue(charDigits[i])));
-
         }
         return nums;
     }
 
+    public static boolean isValidInput (String input, int base_in){
+        boolean valid = true;
+        if (input.matches("[^a-z0-9]")){
+            System.out.println("input non alpha-numeric or contains uppercase letters");
+            valid = false;
+        }
+        if (input.matches(".*\\s+.*")){
+            System.out.println("input contains spaces");
+            valid = false;
+        }
+        if (input.length() == 0){
+            System.out.println("input length zero");
+            valid = false;
+        }
+        if (input == null){
+            System.out.println("input string is null");
+            valid = false;
+        }
 
+      /*  BigInteger input_as_int = toInteger(convertIntoArray(input));
+        BigInteger base_in_as_big_int = BigInteger.valueOf(base_in);
+        if (input_as_int.hashCode() > base_in_as_big_int.hashCode()){
+            System.out.println("input greater than base");
+            valid = false;
+        }*/
+        return valid;
+    }
 
     public static void main(String[] args) {
-        System.out.println(convert("123", 10, 36));
+        System.out.println(convert("af", 10, 2));
     }
 
 }
-
-
- /*  int sum = 0;
-        int tempPow = -1;
-        ArrayList<Integer> powers = new ArrayList<Integer>();
-
-        for (int i = 0; i < nums.size(); i++) {
-            tempPow = (int)Math.pow(base_in, i);
-            if (tempPow == 0){
-                powers.add(0);
-            }
-            else {
-                powers.add(tempPow);
-            }
-        }
-        for (int i = 0; i < powers.size(); i++) {
-            sum += powers.get(i);
-        }
-
-        return sum;*/
